23b50b78b7c08952c21aae7d67a164943664c59b
magnusingason <80974014+magnusingason@users.noreply.github.com>
2022-11-30 15:58:05 +0000

    readme

--- python discord bot/bot.py
+++ python discord bot/bot.py
@@ -6,24 +6,33 @@ import os
 
 
 def run_discord_bot():
-    TOKEN = 'MTA0MTc2NzUyNjcwMDQzMzU1OQ.G_g0bt.NA68F4J-xnl49TwmcWDnJe_iWD6qmM2nsj6uHk'
+    TOKEN = 'MTA0MTc2NzUyNjcwMDQzMzU1OQ.G_g0bt.NA68F4J-xnl49TwmcWDnJe_iWD6qmM2nsj6uHk' #you need to place your discord bot token here
     bot = commands.Bot(intents=discord.Intents.all(),command_prefix='!')
 
-
+    #help command
     @bot.command()
     async def info(ctx):
         await ctx.send("use !sendtoaddress [address] [amount] to tip someone!")
 
+    #tipping command
     @bot.command()
     async def sendtoaddress(ctx, arg1, arg2):
+        
+        #command for linux to execute
         x = os.system(f'smileycoin-cli sendtoaddress {arg1} {arg2}')
-        await ctx.send(x)
+
+        #in this case linux has given the error code 256 which means that it could not proccess the json (something wrong with the funds value)
         if x == 256:
             await ctx.send(f'Error: Could not connect to server, not enough funds or could not process fund: {arg2}.')
+        #in this case linux sends the error code 1280 indicating that the address given is incorrect
         elif x == 1280:
             await ctx.send(f'Error: Incorrect address: {arg1}.')
+
+        #in this case linux has given the error code 32512 indicating that there is an error with the bash command (most likely server has not been connected to)
         elif x == 32512:
             await ctx.send(f'Linux error.')
+
+        #everything works as it should
         elif x == 0:
             await ctx.send(f'{ctx.author} sent {arg2} to: {arg1}')
 
